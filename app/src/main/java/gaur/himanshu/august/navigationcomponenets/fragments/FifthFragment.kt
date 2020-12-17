276c0b7c3a82b8e06569d114ea3ebcc7172a4d98
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2020-12-18 00:08:43 +0530

    Bottom Navigation View

--- app/src/main/java/gaur/himanshu/august/navigationcomponenets/fragments/FifthFragment.kt
+++ app/src/main/java/gaur/himanshu/august/navigationcomponenets/fragments/FifthFragment.kt
@@ -1,11 +1,13 @@
 package gaur.himanshu.august.navigationcomponenets.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import gaur.himanshu.august.navigationcomponenets.R
+import kotlinx.android.synthetic.main.fragment_fifth.view.*
 
 class FifthFragment : Fragment() {
 
@@ -19,7 +21,17 @@ class FifthFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_fifth, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.pop_up.setOnClickListener {
+            findNavController().navigate(R.id.action_fifthFragment_to_sheetFragment)
+        }
     }
 
 
